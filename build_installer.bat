db01c1ad5323424a6aa66c579f28b2eb5f905929
LHY0125 <3364451258@qq.com>
2026-03-19 13:22:19 +0800

    chore: 移除构建安装程序前的DLL复制步骤
    不再需要手动复制IUP DLL文件到bin目录，因为安装程序脚本已直接引用libs目录中的文件。

--- build_installer.bat
+++ build_installer.bat
@@ -1,7 +1,4 @@
 @echo off
-echo Copying DLLs...
-if not exist bin mkdir bin
-copy /Y "libs\iup-3.31_Win64_dllw6_lib\*.dll" bin\
 
 echo Building Installer...
 "D:\Program Files (x86)\Inno Setup 6\ISCC.exe" "dist\installer.iss"
